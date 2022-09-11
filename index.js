6b1fc836e00a53f3d5b9bc74917e68603628b0b7
leomdsousa <leonardomdsousa@gmail.com>
2022-09-11 15:57:55 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var somar = (n1, n2) => n1 + n2;
+var subtrair = (n1, n2) => n1 - n2;
+var multiplicar = (n1, n2) => n1 * n2;
+var dividir = (n1, n2) => n1 / n2;
+
+module.exports = {
+    somar: somar(),
+    subtrair: subtrair(),
+    multiplicar: multiplicar(),
+    dividir: dividir()
+}
